1ce3505d9278d81296c0fe6558b854efa53269b0
Kevin Menard <nirvdrum@gmail.com>
2012-10-15 20:29:21 -0400

    Consolidate Regexp matches in node mapping.

--- lib/psych/visitors/to_ruby.rb
+++ lib/psych/visitors/to_ruby.rb
@@ -136,100 +136,73 @@ module Psych
         end
       end
 
+=begin
+      def complex match
+        p :complex => match
+      end
+
+      def rational match
+        p :rational => match
+      end
+
+      def object match
+        p :object => [match, (match.string.split(':')[1] || "Object")]
+      end
+
+      METHODS = {
+          '!ruby/object:Complex'  => :complex,
+          '!ruby/object:Rational' => :rational,
+          '!ruby/object'          => :object,
+      }
+
+      re = %r{
+  ^!ruby/object:(?:Complex|Rational)$ |
+  ^!ruby/object
+}x
+
+      [
+          '!ruby/object:Complex',
+          '!ruby/object:Rational',
+          '!ruby/object:MyObject',
+          '!ruby/object',
+      ].each do |str|
+        match = re.match(str)
+        send METHODS[match.to_s], match
+      end
+=end
+
+      REVIVE = {
+        '!ruby/struct' => :revive_struct,
+        '!ruby/array' => :revive_array,
+        '!ruby/object' => :revive_object,
+        '!ruby/exception' => :revive_exception,
+        '!ruby/hash' => :revive_hash,
+        '!ruby/object:Complex' => :revive_complex,
+        '!ruby/object:Rational' => :revive_rational
+      }
+
       def visit_Psych_Nodes_Mapping o
         return revive(Psych.load_tags[o.tag], o) if Psych.load_tags[o.tag]
         return revive_hash({}, o) unless o.tag
 
         case o.tag
-        when /^!ruby\/struct:?(.*)?$/
-          klass = resolve_class($1)
-
-          if klass
-            s = register(o, klass.allocate)
-
-            members = {}
-            struct_members = s.members.map { |x| x.to_sym }
-            o.children.each_slice(2) do |k,v|
-              member = accept(k)
-              value  = accept(v)
-              if struct_members.include?(member.to_sym)
-                s.send("#{member}=", value)
-              else
-                members[member.to_s.sub(/^@/, '')] = value
-              end
-            end
-            init_with(s, members, o)
-          else
-            members = o.children.map { |c| accept c }
-            h = Hash[*members]
-            Struct.new(*h.map { |k,v| k.to_sym }).new(*h.map { |k,v| v })
-          end
-
-        when /^!ruby\/object:?(.*)?$/
-          name = $1 || 'Object'
-
-          if name == 'Complex'
-            h = Hash[*o.children.map { |c| accept c }]
-            register o, Complex(h['real'], h['image'])
-          elsif name == 'Rational'
-            h = Hash[*o.children.map { |c| accept c }]
-            register o, Rational(h['numerator'], h['denominator'])
-          else
-            obj = revive((resolve_class(name) || Object), o)
-            obj
-          end
+        when /^(!ruby\/(?:array|struct|object|exception)):?(.*)$/
+          send REVIVE.key?("#{$1}:#{$2}") ? REVIVE["#{$1}:#{$2}"] : REVIVE[$1], o, $2
 
         when /^!(?:str|ruby\/string)(?::(.*))?/, 'tag:yaml.org,2002:str'
-          klass = resolve_class($1)
-          members = Hash[*o.children.map { |c| accept c }]
-          string = members.delete 'str'
-
-          if klass
-            string = klass.allocate.replace string
-            register(o, string)
-          end
-
-          init_with(string, members.map { |k,v| [k.to_s.sub(/^@/, ''),v] }, o)
-        when /^!ruby\/array:(.*)$/
-          klass = resolve_class($1)
-          list  = register(o, klass.allocate)
-
-          members = Hash[o.children.map { |c| accept c }.each_slice(2).to_a]
-          list.replace members['internal']
-
-          members['ivars'].each do |ivar, v|
-            list.instance_variable_set ivar, v
-          end
-          list
+          revive_string(o, $1)
 
         when '!ruby/range'
-          h = Hash[*o.children.map { |c| accept c }]
-          register o, Range.new(h['begin'], h['end'], h['excl'])
-
-        when /^!ruby\/exception:?(.*)?$/
-          h = Hash[*o.children.map { |c| accept c }]
-
-          e = build_exception((resolve_class($1) || Exception),
-                              h.delete('message'))
-          init_with(e, h, o)
+          revive_range(o)
 
         when '!set', 'tag:yaml.org,2002:set'
-          set = Psych::Set.new
-          @st[o.anchor] = set if o.anchor
-          o.children.each_slice(2) do |k,v|
-            set[accept(k)] = accept(v)
-          end
-          set
+          revive_set(o)
 
         when /^!map:(.*)$/, /^!ruby\/hash:(.*)$/
           revive_hash resolve_class($1).new, o
 
         when '!omap', 'tag:yaml.org,2002:omap'
-          map = register(o, Psych::Omap.new)
-          o.children.each_slice(2) do |l,r|
-            map[accept(l)] = accept r
-          end
-          map
+          revive_map(o)
 
         else
           revive_hash({}, o)
@@ -326,6 +299,104 @@ module Psych
           end
           raise retried
         end
+      end
+
+      def revive_struct object, match
+        klass = resolve_class(match)
+
+        if klass
+          s = register(object, klass.allocate)
+
+          members = {}
+          struct_members = s.members.map { |x| x.to_sym }
+          object.children.each_slice(2) do |k,v|
+            member = accept(k)
+            value  = accept(v)
+            if struct_members.include?(member.to_sym)
+              s.send("#{member}=", value)
+            else
+              members[member.to_s.sub(/^@/, '')] = value
+            end
+          end
+          init_with(s, members, object)
+        else
+          members = object.children.map { |c| accept c }
+          h = Hash[*members]
+          Struct.new(*h.map { |k,v| k.to_sym }).new(*h.map { |k,v| v })
+        end
+      end
+
+      def revive_object object, match
+        name = match || 'Object'
+
+        obj = revive((resolve_class(name) || Object), object)
+        obj
+      end
+
+      def revive_complex object, match
+        h = Hash[*object.children.map { |c| accept c }]
+        register object, Complex(h['real'], h['image'])
+      end
+
+      def revive_rational object, match
+        h = Hash[*object.children.map { |c| accept c }]
+        register object, Rational(h['numerator'], h['denominator'])
+      end
+
+      def revive_string object, match
+        klass = resolve_class(match)
+        members = Hash[*object.children.map { |c| accept c }]
+        string = members.delete 'str'
+
+        if klass
+          string = klass.allocate.replace string
+          register(object, string)
+        end
+
+        init_with(string, members.map { |k,v| [k.to_s.sub(/^@/, ''),v] }, object)
+      end
+
+      def revive_array object, match
+        klass = resolve_class(match)
+        list  = register(object, klass.allocate)
+
+        members = Hash[object.children.map { |c| accept c }.each_slice(2).to_a]
+        list.replace members['internal']
+
+        members['ivars'].each do |ivar, v|
+          list.instance_variable_set ivar, v
+        end
+        list
+      end
+
+      def revive_range object
+        h = Hash[*object.children.map { |c| accept c }]
+        register object, Range.new(h['begin'], h['end'], h['excl'])
+      end
+
+      def revive_exception object, match
+        h = Hash[*object.children.map { |c| accept c }]
+
+        e = build_exception((resolve_class(match) || Exception),
+                            h.delete('message'))
+        init_with(e, h, object)
+      end
+
+      def revive_set object
+        set = Psych::Set.new
+        @st[object.anchor] = set if object.anchor
+        object.children.each_slice(2) do |k,v|
+          set[accept(k)] = accept(v)
+        end
+        set
+      end
+
+      def revive_map object
+        map = register(object, Psych::Omap.new)
+        object.children.each_slice(2) do |l,r|
+          map[accept(l)] = accept r
+        end
+        map
       end
     end
   end
